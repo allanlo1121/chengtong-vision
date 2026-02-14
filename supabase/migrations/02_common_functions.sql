d1f8b0cae1cd3cf4b438dc1b2019afdd1d7fe831
AllanLo <allanlo@msn.com>
2026-02-14 20:19:05 +0800

    feater: supabase

---
+++ supabase/migrations/02_common_functions.sql
@@ -0,0 +1,11 @@
+-- ================================
+-- 02_common_functions.sql
+-- ================================
+
+create or replace function set_updated_at()
+returns trigger as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$ language plpgsql;
---
+++ supabase/migrations/2026021301_init.sql
@@ -0,0 +1,7 @@
+-- ================================
+-- 01_extensions.sql
+-- ================================
+
+create extension if not exists "uuid-ossp";
+create extension if not exists pgcrypto;
+create extension if not exists btree_gin;
