a003c1ab749aa26609f2bd067fb2a1dee2a95f97
AllanLo <allanlo@msn.com>
2026-02-25 19:29:17 +0800

    feater: organization

--- supabase/migrations/1500_create_v_organizations.sql
+++ supabase/migrations/1500_create_v_organizations.sql
@@ -27,16 +27,21 @@ create or replace view public.v_organizations_list as
 select
   o.id,
   o.name,
-  o.parent_id,
+  p.name as parent_name,
   o.is_active,
   o.created_at,
 
   t.name as org_type_name,
   b.name as business_name,
-  r.name as region_name
+  r.name as region_name,
+  c.name as country_name,
+  a.name as admin_region_name
 
 from public.organizations o
+left join organizations p on p.id = o.parent_id
 left join public.master_data t on t.id = o.org_type_id
 left join public.master_data b on b.id = o.business_id
 left join public.master_data r on r.id = o.region_id
+left join public.countries c on c.code = o.country_code
+left join public.admin_regions a on a.code = o.admin_region_code
 where o.deleted_at is null;
