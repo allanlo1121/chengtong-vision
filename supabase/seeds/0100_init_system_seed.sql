ba7277d8b25449a91bf2fd3b251182146ae0f17d
AllanLo <allanlo@msn.com>
2026-02-27 22:35:57 +0800

    feater: menu

--- supabase/seeds/0100_init_system_seed.sql
+++ supabase/seeds/0100_init_system_seed.sql
@@ -1,6 +1,6 @@
-insert into system.menus (label, path, icon, sort_order, group_name, permission_code)
+insert into system.menus (name,label, path, icon, sort_order, group_name, permission_code)
 values
-('Dashboard','/dashboard','LayoutDashboard',0,'System','dashboard.read'),
-('Organizations','/system/orgs','Building2',1,'System','organization.read'),
-('Employees','/system/employees','Users',2,'System','employee.read'),
-('Projects','/projects','FolderKanban',1,'Project','project.read');
+('dashboard','Dashboard','/dashboard','LayoutDashboard',0,'System','dashboard.read'),
+('organizations','Organizations','/system/orgs','Building2',1,'System','organization.read'),
+('employees','Employees','/system/employees','Users',2,'System','employee.read'),
+('projects','Projects','/projects','FolderKanban',1,'Project','project.read');
